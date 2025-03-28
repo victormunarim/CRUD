d3355701605c024329568307ae0932c212041cc4
victormunarim <victor.eduardo.munarim@hotmail.com>
2025-03-28 00:31:22 -0300

    finalizacao da parte de funcionalidade do CRUD

--- database.php
+++ database.php
@@ -4,7 +4,13 @@ $bancodedados = "empresa";
 $usuario = "root";
 $senha = "";
 
-$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
-if ($mysqli->connect_errno) {
-    echo "falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+// $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+// if ($mysqli->connect_errno) {
+//     echo "falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+// }
+
+$conexao = mysqli_connect($hostname, $usuario, $senha, $bancodedados);
+
+if (!$conexao) {
+    die("conexao falhou: " . mysqli_connect_error());
 }
--- index.php
+++ index.php
@@ -14,58 +14,154 @@
         <h1 class="titulo">CRUD para gestão de etiquetas</h1>
     </header>
 
+    <?php
+
+    ?>
+
     <main>
 
         <h2>Cadastro de produto</h2>
 
-        <form action="cadastro.php" method="post">
-            <label for="nome">Nome:</label>
-            <input type="text" name="nome">
-            <label for="preco">Preço:</label>
-            <input type="number" name="preco">
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
+            <label for="nome_cadastro">Nome:</label>
+            <input type="text" name="nome_cadastro">
+            <label for="preco_cadastro">Preço:</label>
+            <input type="number" name="preco_cadastro">
             <input type="submit">
         </form>
 
+        <?php
+        include("database.php");
+
+        if (isset($_GET["nome_cadastro"]) && isset($_GET["preco_cadastro"])) {
+            $nome = $_GET["nome_cadastro"];
+            $preco = $_GET["preco_cadastro"];
+
+
+            $sql_code = "INSERT INTO etiquetas (preco, nome)
+                VALUES ($preco, '$nome');";
+
+            if (mysqli_query($conexao, $sql_code)) {
+                echo "<p>Registrado produto $nome</p>";
+            } else {
+                echo "<p>Produto não registrado</p>";
+            }
+        }
+
+
+        ?>
+
         <h2>Leitura de produto</h2>
-        
+
         <form action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
-            <select name="produto">
+            <select name="produto_leitura">
+                <option selected>Selecione</option>
                 <?php
                 include("database.php");
 
                 $sql_code = "SELECT * FROM etiquetas";
 
-                $sql_query = $mysqli->query($sql_code) or die("erro: " . $mysqli->error);
-
-                while ($dados = $sql_query->fetch_assoc()) {
-                    echo "<option value='$dados[nome]'>$dados[nome]</option>";
+                if (mysqli_query($conexao, $sql_code)) {
+                    $dados = mysqli_query($conexao, $sql_code);
+                    while ($linha = mysqli_fetch_assoc($dados)) {
+                        echo "<option value='$linha[nome]'>$linha[nome]</option>";
+                    }
                 }
+
                 ?>
             </select>
             <input type="submit">
         </form>
 
         <?php
-        $produtoEscolhido = $_GET["produto"];
-        echo "<p>$produtoEscolhido</p>";
+
+        if (isset($_GET["produto_leitura"])) {
+            $produtoEscolhido = $_GET["produto_leitura"];
+            $sql_code = "SELECT * FROM etiquetas WHERE nome = '$produtoEscolhido'";
+
+            if (mysqli_query($conexao, $sql_code)) {
+                $dados = mysqli_query($conexao, $sql_code);
+                $linha = mysqli_fetch_assoc($dados);
+                if (isset($linha)) {
+                    echo "<p>ID: $linha[produtoID] - Nome: $linha[nome] - Preco: $linha[preco]</p>";
+                }
+            }
+        }
+
+
         ?>
 
         <h2>Atualização de produto</h2>
 
-        <?php
-        while ($dados = $sql_query->fetch_assoc()) {
-            echo "<p>$dados[nome]</p>";
-            echo "<p>$dados[preco]</p>";
-        }
-        ?>
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
+            <select name="nome_atualizacao">
+                <option selected>Selecione</option>
+                <?php
+                include("database.php");
 
-        <form action="atualizar.php" method="get">
-            <label for="nome">Nome:</label>
-            <input type="text" name="nome">
-            <label for="preco">Preço:</label>
-            <input type="number" name="preco">
+                $sql_code = "SELECT * FROM etiquetas";
+
+                if (mysqli_query($conexao, $sql_code)) {
+                    $dados = mysqli_query($conexao, $sql_code);
+                    while ($linha = mysqli_fetch_assoc($dados)) {
+                        echo "<option value='$linha[nome]'>$linha[nome]</option>";
+                    }
+                }
+
+                ?>
+            </select>
+            <label for="preco_atualizado">Preço:</label>
+            <input type="number" name="preco_atualizado">
             <input type="submit">
         </form>
+
+        <?php
+        if (isset($_GET["preco_atualizado"]) && isset($_GET["nome_atualizacao"])) {
+            $preco_atualizado = $_GET["preco_atualizado"];
+            $nome_atualizacao = $_GET["nome_atualizacao"];
+
+            include("database.php");
+
+            $sql_code = "UPDATE etiquetas SET preco = '$preco_atualizado' WHERE nome = '$nome_atualizacao'";
+
+            if (mysqli_query($conexao, $sql_code)) {
+                echo "<p>Produto $nome_atualizacao atualizado com sucesso</p>";
+            }
+        }
+
+        ?>
+
+        <h2>Deletar produto</h2>
+
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
+            <select name="nome_excluir">
+                <option selected>Selecione</option>
+                <?php
+                include("database.php");
+
+                $sql_code = "SELECT * FROM etiquetas";
+
+                if (mysqli_query($conexao, $sql_code)) {
+                    $dados = mysqli_query($conexao, $sql_code);
+                    while ($linha = mysqli_fetch_assoc($dados)) {
+                        echo "<option value='$linha[nome]'>$linha[nome]</option>";
+                    }
+                }
+
+                ?>
+            </select>
+            <input type="submit">
+        </form>
+        <?php
+        if (isset($_GET["nome_excluir"])) {
+            $produto_excluir = $_GET["nome_excluir"];
+            $sql_code = "DELETE FROM etiquetas WHERE nome = '$produto_excluir'";
+
+            if (mysqli_query($conexao, $sql_code)) {
+                echo "<p>Produto: $produto_excluir excluido com sucesso</p>";
+            }
+        }
+        ?>
 
     </main>
 
